6ad20fdfef602e46c4cb82c528b4547d1a516b29
Abdul Hanan Nawaz <93150331+Hanan-Nawaz@users.noreply.github.com>
2024-01-20 20:47:35 +0500

    about-us.php file uploaded.

---
+++ Web Engineering - 1/NewsDesk/about-us.php
@@ -0,0 +1,82 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>About Us - NewsDesk</title>
+    <link rel="stylesheet" href="assets/css/style.css" />
+    <link rel="icon" type="image/x-icon" href="assets/imgs/NewsDesk.png">
+</head>
+<body>
+
+
+    <header>
+        <div class="container">
+            <div class="col-div-6">
+                <p class="logo"><span>News</span>Desk</p>
+            </div>
+            <div class="col-div-6">
+                <ul class="nav">
+                    <li><a href="index.php">Blog</a></li>
+                    <li><a href="index.php#latest">Latest</a></li>
+                    <li><a href="contact.php">Contact</a></li>
+                    <li><a href="about-us.php">About Us</a></li>
+                    <li><a href="our-team.php">Our Team</a></li>
+                    <li><a class="buttons" href="auth.php">Join US</a></li>
+                </ul>
+            </div>
+            <div class="clearfix"></div>
+        </div>
+    </header>
+
+    <div class="we-are-block">
+
+        <div id="about-us-section">
+      
+          <div class="about-us-image">
+      
+            <img src="https://digitalupgrade.com/images/lobbyimage_1.jpg" width="808" height="458" alt="Lobby Image">      
+          </div>
+      
+          <div class="about-us-info">
+      
+            <h2>We are Digital Upgrade</h2>
+      
+            <p>Digital Upgrade, located in Evansville, IN, makes it easy for businesses to create and manage their digital presence and logistics, meaning we can make your business more efficient and profitable! We believe in our community and want to help our local business economy grow and expand. We believe in our clients, and want to assist in their digital success. The D-UP team grades ourselves on outcomes, not on sales.</p>
+      
+
+          </div>
+      
+        </div>
+      
+        <div id="history-section">
+      
+          <div class="history-image">
+      
+            <img src="https://digitalupgrade.com/images/building_pic.jpg" width="951" height="471" alt="Building Pic">
+      
+          </div>
+      
+          <div class="history-info">
+      
+            <h2>Preserving Local History</h2>
+      
+            <p>In 2018, technology companies, Desktop Doctors & Digital Upgrade found a new place to call home, at 816 N. 9th Ave. Since the takeover of the 118-year-old building (formally Stippler Tool & Supply and Crown Chair Company), there have been many renovations completed to preserve this local piece of Evansville, IN history.</p>
+      
+      
+          </div>
+      
+        </div>
+      
+      </div>
+
+
+      
+      <footer>
+        <div class="container">
+          <p>Copyright@ News Desk | Developed by <a href="https://hanannawaz.com" style="color: rgb(115, 146, 173);">Abdul Hanan Nawaz</a> & Team</p>
+        </div>
+      </footer>
+</body>
+</html>
