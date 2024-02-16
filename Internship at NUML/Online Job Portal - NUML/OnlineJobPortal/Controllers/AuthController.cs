d9c7bbfc1e558fbf674c7022086c446d506c072a
Abdul Hanan Nawaz <93150331+Hanan-Nawaz@users.noreply.github.com>
2024-02-16 17:58:16 +0500

    Auth Controller Uploaded.

---
+++ Internship at NUML/Online Job Portal - NUML/OnlineJobPortal/Controllers/AuthController.cs
@@ -0,0 +1,188 @@
+﻿using OnlineJobPortal.DbContect;
+using OnlineJobPortal.Models;
+using OnlineJobPortal.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace OnlineJobPortal.Controllers
+{
+    public class AuthController : Controller
+    {
+        private context db = new context();
+
+        public ActionResult SignUp()
+        {
+            return View();
+        }
+
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult SignUp(JobGuestUsers jobGuestUsers)
+        {
+            bool isUnique = db.userinfo.All(u => u.CNIC != jobGuestUsers.CNIC && u.CellPhone != jobGuestUsers.CellPhone && u.Email != jobGuestUsers.Email);
+
+            if(isUnique)
+            {
+                db.userinfo.Add(jobGuestUsers);
+                int affectedRows = db.SaveChanges();
+
+                if (affectedRows > 0)
+                {
+                    string userToken = Guid.NewGuid().ToString();
+
+                    GuestUserToken guestUserToken = new GuestUserToken();
+
+                    guestUserToken.AuthToken = userToken + jobGuestUsers.GuestID;
+                    guestUserToken.IssuedOn = DateTime.Now;
+                    guestUserToken.UserID = jobGuestUsers.GuestID.ToString();
+                    guestUserToken.ExpiresOn = DateTime.Now.AddMinutes(30);
+
+                    db.userToken.Add(guestUserToken);
+                    int rowsAffected = db.SaveChanges();
+
+                    if(rowsAffected > 0)
+                    {
+                        TempData["SuccessMessage"] = "Signed up successfully.";
+                        return RedirectToAction("Login");
+                    }
+                    else
+                    {
+                        JobGuestUsers userToRemove = db.userinfo.Find(jobGuestUsers.GuestID);
+                        db.userinfo.Remove(userToRemove);
+                        db.SaveChanges();
+                        TempData["SuccessMessage"] = "Error! try Again.";
+
+                        return RedirectToAction("SignUp");
+                    }
+                }
+                else
+                {
+                    TempData["SuccessMessage"] = "Error! try Again.";
+                    return RedirectToAction("SignUp");
+                }
+            }
+            else
+            {
+                TempData["SuccessMessage"] = "User already exist. Please Login";
+                return RedirectToAction("SignUp");
+            }
+
+
+
+        }
+
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Login(JobGuestUsers jobGuestUsers)
+        {
+            bool isUnique = db.userinfo.Any(u => u.Email == jobGuestUsers.Email && u.Password == jobGuestUsers.Password);
+
+            if (isUnique)
+            {
+                JobGuestUsers loggedInUser = db.userinfo.FirstOrDefault(u => u.Email == jobGuestUsers.Email && u.Password == jobGuestUsers.Password);
+                GuestUserToken guestUserToken = db.userToken.FirstOrDefault(u => u.UserID == jobGuestUsers.GuestID.ToString());
+
+                HttpCookie userInfoCookie = new HttpCookie("UserInfo");
+
+                if(guestUserToken != null)
+                {
+                    db.userToken.Remove(guestUserToken);
+                    db.SaveChanges();
+                }
+
+                string userToken = Guid.NewGuid().ToString();
+
+                GuestUserToken guestUserTokens = new GuestUserToken();
+
+                guestUserTokens.AuthToken = userToken + loggedInUser.GuestID;
+                guestUserTokens.IssuedOn = DateTime.Now;
+                guestUserTokens.UserID = loggedInUser.GuestID.ToString();
+                guestUserTokens.ExpiresOn = DateTime.Now.AddMinutes(30);
+
+                db.userToken.Add(guestUserTokens);
+                int rowsAffected = db.SaveChanges();
+
+
+                // Add data to the cookie
+                userInfoCookie["UserID"] = loggedInUser.GuestID.ToString();
+                userInfoCookie["Token"] = guestUserTokens.TokenID.ToString();
+                userInfoCookie["ExpiresOn"] = guestUserTokens.ExpiresOn.ToString();
+
+                // Set the expiration time for the cookie
+                userInfoCookie.Expires = DateTime.Now.AddHours(3); // Example: Expires in 3 hour
+
+                // Add the cookie to the response
+                Response.Cookies.Add(userInfoCookie);
+
+                return RedirectToAction("AddPublication", "JobPublications");
+            }
+            else
+            {
+                TempData["SuccessMessage"] = "User don't exist. Please SignUp";
+                return RedirectToAction("Login");
+            }
+        }
+
+        public ActionResult ValidateJobToken()
+        {
+            HttpCookie userInfoCookie = Request.Cookies["UserInfo"];
+
+            // Check if the cookie exists and if it contains the "UserID" key
+            if (userInfoCookie != null && userInfoCookie["UserID"] != null)
+            {
+                int userId;
+                if (int.TryParse(userInfoCookie["UserID"], out userId)) { 
+
+                    GuestUserToken guestUserToken = db.userToken.FirstOrDefault(u => u.UserID == userId.ToString());
+
+                    if (guestUserToken.ExpiresOn > DateTime.Now)
+                    {
+                        guestUserToken.ExpiresOn = guestUserToken.ExpiresOn.AddMinutes(30);
+                        db.SaveChanges();
+                        return null;
+
+                    }
+                    else
+                    {
+                        guestUserToken.ExpiresOn = guestUserToken.ExpiresOn.AddMinutes(30);
+                        db.SaveChanges();
+                        return null;
+                    }
+
+                }
+                return null;
+
+            }
+            else
+            {
+                return RedirectToAction("Login", "Auth"); // Redirect to the login page
+            }
+
+        }
+
+
+        public ActionResult Logout()
+        {
+            // Expire the authentication cookie
+            HttpCookie userInfoCookie = HttpContext.Response.Cookies["UserInfo"];
+            if (userInfoCookie != null)
+            {
+                userInfoCookie.Expires = DateTime.Now.AddDays(-1);
+                HttpContext.Response.Cookies.Add(userInfoCookie);
+            }
+
+            // Redirect to the login page
+            return RedirectToAction("Login", "Auth");
+        }
+    }
+}
---
+++ Internship at NUML/Online Job Portal - NUML/OnlineJobPortal/Controllers/JobPublicationsController.cs
@@ -0,0 +1,195 @@
+﻿using OnlineJobPortal.DbContect;
+using OnlineJobPortal.Models;
+using OnlineJobPortal.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+
+namespace OnlineJobPortal.Controllers
+{
+    public class JobPublicationsController : AuthController
+    {
+        private context db = new context();
+
+        // GET: Publications/Details/5
+        public ActionResult View(int? id)
+        {
+            ActionResult validationResult = ValidateJobToken();
+
+            // Check if validation failed and return the result if it did
+            if (validationResult != null)
+            {
+                return validationResult;
+            }
+
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            JobPublications publication = db.JobPublications.Find(id);
+            if (publication == null)
+            {
+                return HttpNotFound();
+            }
+            return View(publication);
+        }
+
+        // GET: Publications/Create
+        public ActionResult AddPublication()
+        {
+            ActionResult validationResult = ValidateJobToken();
+
+            // Check if validation failed and return the result if it did
+            if (validationResult != null)
+            {
+                return validationResult;
+            }
+
+            IEnumerable<JobPublications> existingPublications = db.JobPublications.OrderByDescending(pub => pub.publication_id).ToList();
+
+            PublicationVM viewModel = new PublicationVM
+            {
+                existingPublications = existingPublications,
+                jobPublication = new JobPublications()
+            };
+
+
+
+            if (TempData["SuccessMessage"] != null)
+            {
+                ViewBag.SuccessMessage = TempData["SuccessMessage"].ToString();
+                ViewBag.alertMessage = "";
+            }
+            else
+            {
+                ViewBag.SuccessMessage = "";
+                ViewBag.alertMessage = "";
+            }
+
+            return View(viewModel);
+        }
+
+        // POST: Publications/Create        
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult AddPublication(PublicationVM viewModel)
+        {
+            ActionResult validationResult = ValidateJobToken();
+
+            // Check if validation failed and return the result if it did
+            if (validationResult != null)
+            {
+                return validationResult;
+            }
+            viewModel.existingPublications = db.JobPublications.ToList(); // Retrieve existing publications again if needed
+
+            if (ModelState.IsValid)
+            {
+                // Add the received publication model to the database context
+                db.JobPublications.Add(viewModel.jobPublication);
+
+                // Save changes to the database
+                db.SaveChanges();
+
+                // Redirect to a different action after successful addition
+                TempData["SuccessMessage"] = "Publication added successfully.";
+                return RedirectToAction("AddPublication"); // Replace "SuccessAction" with your action name
+            }
+
+            // If ModelState is not valid, reload the view with the received PublicationVM model to show validation errors
+            return View(viewModel);
+        }
+
+        // GET: Publications/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            ActionResult validationResult = ValidateJobToken();
+
+            // Check if validation failed and return the result if it did
+            if (validationResult != null)
+            {
+                return validationResult;
+            }
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            JobPublications publication = db.JobPublications.Find(id);
+            if (publication == null)
+            {
+                return HttpNotFound();
+            }
+            return View(publication);
+        }
+
+        // POST: Publications/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, JobPublications publication)
+        {
+            ActionResult validationResult = ValidateJobToken();
+
+            // Check if validation failed and return the result if it did
+            if (validationResult != null)
+            {
+                return validationResult;
+            }
+
+            if (ModelState.IsValid)
+            {
+                // Update the received publication model in the database context
+                db.Entry(publication).State = EntityState.Modified;
+
+                // Save changes to the database
+                db.SaveChanges();
+
+                TempData["SuccessMessage"] = "Publication updated successfully.";
+
+                // Redirect to a different action after successful update
+                return RedirectToAction("AddPublication"); // Replace "SuccessAction" with your action name
+            }
+
+            return View(publication);
+        }
+
+        // POST: Publications/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            ActionResult validationResult = ValidateJobToken();
+
+            // Check if validation failed and return the result if it did
+            if (validationResult != null)
+            {
+                return validationResult;
+            }
+
+            JobPublications publication = db.JobPublications.Find(id);
+            if (publication == null)
+            {
+                return HttpNotFound();
+            }
+
+            db.JobPublications.Remove(publication);
+            db.SaveChanges();
+            TempData["SuccessMessage"] = "Publication deleted successfully.";
+
+            return RedirectToAction("AddPublication"); // Redirect to list view after deletion
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+    }
+}
