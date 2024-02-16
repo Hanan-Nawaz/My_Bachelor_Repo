1e8f160275916e9f445b5c0205f2aebf6ef1a877
Abdul Hanan Nawaz <93150331+Hanan-Nawaz@users.noreply.github.com>
2024-02-16 17:59:30 +0500

    Migrations Added.

---
+++ Internship at NUML/Online Job Portal - NUML/OnlineJobPortal/Migrations/202402161110359_Publications added.cs
@@ -0,0 +1,36 @@
+﻿namespace OnlineJobPortal.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class Publicationsadded : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.JobPublications",
+                c => new
+                    {
+                        publication_id = c.Int(nullable: false, identity: true),
+                        author_name = c.String(nullable: false, maxLength: 200, unicode: false),
+                        journal_name = c.String(nullable: false, maxLength: 200, unicode: false),
+                        journal_address = c.String(nullable: false, maxLength: 8000, unicode: false),
+                        issn_number = c.String(nullable: false, maxLength: 30, unicode: false),
+                        publication_title = c.String(maxLength: 30, unicode: false),
+                        vol_no = c.String(nullable: false, maxLength: 8000, unicode: false),
+                        start_page = c.Int(nullable: false),
+                        end_page = c.Int(nullable: false),
+                        hec_category = c.String(nullable: false, maxLength: 8000, unicode: false),
+                        published_year = c.String(nullable: false, maxLength: 200, unicode: false),
+                        impact_factor = c.Single(nullable: false),
+                    })
+                .PrimaryKey(t => t.publication_id);
+            
+        }
+        
+        public override void Down()
+        {
+            DropTable("dbo.JobPublications");
+        }
+    }
+}
---
+++ Internship at NUML/Online Job Portal - NUML/OnlineJobPortal/Migrations/202402161143008_tables added.cs
@@ -0,0 +1,47 @@
+﻿namespace OnlineJobPortal.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class tablesadded : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.JobGuestUsers",
+                c => new
+                    {
+                        GuestID = c.Int(nullable: false, identity: true),
+                        FirstName = c.String(nullable: false),
+                        LastName = c.String(nullable: false),
+                        CNIC = c.String(nullable: false),
+                        Gender = c.String(nullable: false),
+                        DOB = c.DateTime(nullable: false),
+                        CellPhone = c.String(nullable: false),
+                        Email = c.String(nullable: false),
+                        Password = c.String(nullable: false),
+                        Type = c.String(nullable: false),
+                    })
+                .PrimaryKey(t => t.GuestID);
+            
+            CreateTable(
+                "dbo.GuestUserTokens",
+                c => new
+                    {
+                        TokenID = c.Int(nullable: false, identity: true),
+                        AuthToken = c.String(nullable: false),
+                        UserID = c.String(nullable: false),
+                        IssuedOn = c.DateTime(nullable: false),
+                        ExpiresOn = c.DateTime(nullable: false),
+                    })
+                .PrimaryKey(t => t.TokenID);
+            
+        }
+        
+        public override void Down()
+        {
+            DropTable("dbo.GuestUserTokens");
+            DropTable("dbo.JobGuestUsers");
+        }
+    }
+}
