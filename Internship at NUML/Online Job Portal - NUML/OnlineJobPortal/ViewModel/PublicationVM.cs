ec81dcc392ff93c8db317ecceb7cc468579479b0
Abdul Hanan Nawaz <93150331+Hanan-Nawaz@users.noreply.github.com>
2024-02-16 18:02:35 +0500

    View Model Uploaded for Online Job Portal -NUML

---
+++ Internship at NUML/Online Job Portal - NUML/OnlineJobPortal/ViewModel/PublicationVM.cs
@@ -0,0 +1,14 @@
+﻿using OnlineJobPortal.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace OnlineJobPortal.ViewModel
+{
+    public class PublicationVM
+    {
+        public IEnumerable<JobPublications> existingPublications { get; set; }
+        public JobPublications jobPublication { get; set; }
+    }
+}
