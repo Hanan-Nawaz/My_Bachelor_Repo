8d5cb1f02b1401b480b6cada1ff1bf83219bae50
Abdul Hanan Nawaz <93150331+Hanan-Nawaz@users.noreply.github.com>
2024-02-27 00:34:44 +0500

    Models Updated.

---
+++ Internship at NUML/Online Job Portal - NUML/OnlineJobPortal/Models/JobPaperhecJournals.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace OnlineJobPortal.Models
+{
+    public class JobPaperhecJournals
+    {
+        [Key]
+        public int id { get; set; }
+
+        [Required]
+        public int w_paper { get; set; }
+
+        [Required]
+        public int x_paper { get; set; }
+
+        [Required]
+        public int y_paper { get; set; }
+
+        [Required]
+        public int z_paper { get; set; }
+
+        [Required]
+        public int total { get; set; }
+
+        [Required]
+        public int last_5_years { get; set; }
+
+        [Required]
+        public int total_hec_approved { get; set; }
+
+        [Required]
+        public int total_hec_not_approved { get; set; }
+    }
+}
---
+++ Internship at NUML/Online Job Portal - NUML/OnlineJobPortal/Models/JobPostPhdExp.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace OnlineJobPortal.Models
+{
+    public class JobPostPhdExp
+    {
+        [Key]
+        public int id { get; set; }
+
+        [Required]
+        public string institute { get; set; }
+
+        [Required]
+        public string position_held { get; set; }
+
+        [Required]
+        public string status { get; set; }
+
+        [Required]
+        public DateTime period_from { get; set; }
+
+        [Required]
+        public DateTime period_to { get; set; }
+
+        [Required]
+        public int exp_year { get; set; }
+
+        [Required]
+        public int exp_month { get; set; }
+
+    }
+}
