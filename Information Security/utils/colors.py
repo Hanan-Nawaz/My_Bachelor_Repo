1ea47608a41a1fa3aef6766431d81b4efc442769
Abdul Hanan Nawaz <93150331+Hanan-Nawaz@users.noreply.github.com>
2024-08-02 17:48:08 +0500

    colors.py coded.

---
+++ Information Security/utils/colors.py
@@ -0,0 +1,16 @@
+class colors:
+    BLACK = '\033[30m'
+    RED = '\033[31m'
+    GREEN = '\033[32m'
+    YELLOW = '\033[33m'
+    BLUE = '\033[34m'
+    MAGENTA = '\033[35m'
+    CYAN = '\033[36m'
+    WHITE = '\033[37m'
+    UNDERLINE = '\033[4m'
+    RESET = '\033[0m'
+
+    yellow_unfilled_bullet = f"{YELLOW}○{RESET}"
+    yellow_filled_bullet = f"{YELLOW}●{RESET}"
+    green_filled_bullet = f"{GREEN}●{RESET}"
+    blue_filled_bullet = f"{BLUE}●{RESET}"
